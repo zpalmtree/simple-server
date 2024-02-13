8a92cb4f8ed789af9a846ec67d067b910974627c
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2024-02-12 21:28:51 -0500

    Add more endpoints

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,26 @@ import { logger } from './Logger.js';
 const app = express();
 const port = 1736;
 
+const sizeInBytes = 10 * 1024 * 1024; // 10MB
+const buffer = Buffer.alloc(sizeInBytes, '0');
+const largeString = buffer.toString();
+
 app.get('/', (req, res) => {
     res.send({
         time: new Date().toISOString(),
+    });
+});
+
+app.post('/', (req, res) => {
+    res.send({
+        time: new Date().toISOString(),
+    });
+});
+
+app.post('/data', (req, res) => {
+    res.send({
+        time: new Date().toISOString(),
+        data: largeString,
     });
 });
 
